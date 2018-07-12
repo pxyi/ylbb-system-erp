d5a3b558b8737e0fe5c2d83b8eddd22cc0864d81
phuhoang <phuhoang@sina.com>
2018-07-12 18:21:33 +0800

    新增角色完毕

--- src/app/modules/account/role/role.component.ts
+++ src/app/modules/account/role/role.component.ts
@@ -1,10 +1,11 @@
+import { YlbbResponse } from './../../../core/interface-config';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ListPageComponent } from './../../../ng-relax/components/list-page/list-page.component';
 import { HttpClient } from '@angular/common/http';
 import { NzMessageService } from 'ng-zorro-antd';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { TheadNode } from '../../../ng-relax/components/table/table.component';
 import { QueryNode } from '../../../ng-relax/components/query/query.component';
-import { YlbbResponse } from '../../../core/interface-config';
 
 @Component({
   selector: 'app-role',
@@ -32,10 +33,17 @@ export class RoleComponent implements OnInit {
 
   constructor(
     private http : HttpClient,
-    private message: NzMessageService
+    private message: NzMessageService,
+    private fb: FormBuilder = new FormBuilder()
   ) { }
 
   ngOnInit() {
+    this.createRoleForm = this.fb.group({
+      name: [, [Validators.required]],
+      code: [, [Validators.required]],
+      status: [0],
+      mono: []
+    })
   }
 
   editRole(data) {
@@ -58,6 +66,35 @@ export class RoleComponent implements OnInit {
       }
       this.message.create(res.code == 1000 ? 'success' : 'warning', res.info);
     })
+  }
+
+  /* -------------- 新增角色 -------------- */
+  createRoleForm: FormGroup
+  showCreateRole: boolean = false;
+  createLoading: boolean = false;
+  openCreate() {
+    this.showCreateRole = true;
+    this.createRoleForm.reset();
+    this.createRoleForm.patchValue({ status: 0 });
+  }
+
+  crateRole() {
+    if (this.createRoleForm.valid) {
+      this.createLoading = true;
+      this.http.post<YlbbResponse>('/roleManagement/modify', { paramJson: JSON.stringify(this.createRoleForm.value) }).subscribe(res => {
+        if (res.code == 1000) {
+          this.EaListPage.EaTable._request();
+          this.showCreateRole = false;
+          this.createLoading = false;
+        }
+        this.message.create(res.code == 1000 ? 'success' : 'warning', res.info);
+      })
+    } else {
+      for (let i in this.createRoleForm.controls) {
+        this.createRoleForm.controls[i].markAsDirty();
+        this.createRoleForm.controls[i].updateValueAndValidity();
+      }
+    }
   }
 
 }
