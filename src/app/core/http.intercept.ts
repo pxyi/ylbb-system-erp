35be68e130c16c83c378984aaedd4a975ca1e279
phuhoang <phuhoang@sina.com>
2018-07-16 21:23:29 +0800

    HttpClient 拦截器统一展示错误提示

--- src/app/core/http.intercept.ts
+++ src/app/core/http.intercept.ts
@@ -42,6 +42,8 @@ export class NoopInterceptor implements HttpInterceptor {
             if (event.body.code == 3000) {
               window.localStorage.removeItem('userInfo');
               this.router.navigateByUrl('/login');
+            } else if (event.body.code != 1000) {
+              this.message.warning(event.body.info);
             }
           } else {
             this.message.error('网络错误，请刷新重试');
