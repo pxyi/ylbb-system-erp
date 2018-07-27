5fa753bfa59e8d3076d12050606b881909694599
phuhoang <phuhoang@sina.com>
2018-07-27 17:00:44 +0800

    修改测试包地址

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  version: '0.0.0',
-  // domain: 'http://101.200.177.83:7994/new_bss'
-  domain: 'http://192.168.1.138:8080'
+  version: '0.0.1',
+  domain: 'http://101.200.177.83:7994/new_bss'
 };
