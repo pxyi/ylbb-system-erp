b9f42d3abd5b0341ae9213f18a6dc341e9ce4dba
phuhoang <phuhoang@sina.com>
2018-07-27 17:24:24 +0800

    注释权限代码

--- src/app/core/reducers/userInfo-reducer.ts
+++ src/app/core/reducers/userInfo-reducer.ts
@@ -4,10 +4,10 @@ export function userInfoReducer (state: UserInfoState, action: Action) {
   switch (action.type) {
     case 'setUserInfo':
       try {
-        let roleAllowPath = [];
-        action['payload'].roles.map(role => {
-          role.roleJsonInfo && (roleAllowPath = roleAllowPath.concat(role.roleJsonInfo.split(',')));
-        });
+        // let roleAllowPath = [];
+        // action['payload'].roles.map(role => {
+        //   role.roleJsonInfo && (roleAllowPath = roleAllowPath.concat(role.roleJsonInfo.split(',')));
+        // });
         // action['payload']['roleAllowPath'] = Array.from(new Set(roleAllowPath)).join(',');
         action['payload']['roleAllowPath'] = "/home,/home/customer/potential,/home/customer/nointention,/home/visit/clue,/home/visit/nocard,/home/visit/member";
         window.localStorage.setItem('userInfo', JSON.stringify(action['payload']));
