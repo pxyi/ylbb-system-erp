626a1723045c3519934cec3614e2d0cf11374c00
phuhoang <phuhoang@sina.com>
2018-07-30 19:28:38 +0800

    -------- 2018-07-30 ---------

--- src/app/core/menu-config.ts
+++ src/app/core/menu-config.ts
@@ -45,78 +45,78 @@ export const MenuConfig = [
       }
     ]
   },
-  // {
-  //   title : '推广活动',
-  //   key   : '/home/marketing',
-  //   icon  : 'anticon-area-chart',
-  //   children: [
-  //     {
-  //       title : '活动',
-  //       key   : '/home/marketing/activity',
-  //       isLeaf: true
-  //     },
-  //     {
-  //       title: '数据',
-  //       key: '/home/marketing/data',
-  //       isLeaf: true
-  //     }
-  //   ]
-  // },
-  // {
-  //   title : '全国通卡',
-  //   key   : '/home/passcard',
-  //   icon  : 'anticon-idcard',
-  //   isLeaf: true
-  // },
-  // {
-  //   title : '微信小程序',
-  //   key   : '/home/wechat',
-  //   icon  : 'anticon-wechat',
-  //   isLeaf: true
-  // },
-  // {
-  //   title : '充值中心',
-  //   key   : '/home/paymen',
-  //   icon  : 'anticon-pay-circle',
-  //   children: [
-  //     {
-  //       title : '在线充值',
-  //       key   : '/home/payment',
-  //       isLeaf: true
-  //     },
-  //     {
-  //       title: '充值记录',
-  //       key: '/home/payment/record',
-  //       isLeaf: true
-  //     }
-  //   ]
-  // },
-  // {
-  //   title: '账户管理',
-  //   key: '/home/account',
-  //   icon: 'anticon-usergroup-add',
-  //   children: [
-  //     {
-  //       title: '账号管理',
-  //       key: '/home/account/account',
-  //       isLeaf: true
-  //     },
-  //     {
-  //       title: '角色管理',
-  //       key: '/home/account/role',
-  //       isLeaf: true
-  //     },
-  //     {
-  //       title: '登录日志',
-  //       key: '/home/account/loginlog',
-  //       isLeaf: true
-  //     }
-  //   ]
-  // },
-  // {
-  //   title: '用户互动',
-  //   key: '/home/interaction',
-  //   icon: 'anticon-profile',
-  //   isLeaf: true
-  // },
+  {
+    title : '推广活动',
+    key   : '/home/marketing',
+    icon  : 'anticon-area-chart',
+    children: [
+      {
+        title : '活动',
+        key   : '/home/marketing/activity',
+        isLeaf: true
+      },
+      {
+        title: '数据',
+        key: '/home/marketing/data',
+        isLeaf: true
+      }
+    ]
+  },
+  {
+    title : '全国通卡',
+    key   : '/home/passcard',
+    icon  : 'anticon-idcard',
+    isLeaf: true
+  },
+  {
+    title : '微信小程序',
+    key   : '/home/wechat',
+    icon  : 'anticon-wechat',
+    isLeaf: true
+  },
+  {
+    title : '充值中心',
+    key   : '/home/paymen',
+    icon  : 'anticon-pay-circle',
+    children: [
+      {
+        title : '在线充值',
+        key   : '/home/payment',
+        isLeaf: true
+      },
+      {
+        title: '充值记录',
+        key: '/home/payment/record',
+        isLeaf: true
+      }
+    ]
+  },
+  {
+    title: '账户管理',
+    key: '/home/account',
+    icon: 'anticon-usergroup-add',
+    children: [
+      {
+        title: '账号管理',
+        key: '/home/account/account',
+        isLeaf: true
+      },
+      {
+        title: '角色管理',
+        key: '/home/account/role',
+        isLeaf: true
+      },
+      {
+        title: '登录日志',
+        key: '/home/account/loginlog',
+        isLeaf: true
+      }
+    ]
+  },
+  {
+    title: '用户互动',
+    key: '/home/interaction',
+    icon: 'anticon-profile',
+    isLeaf: true
+  },
 ];
--- src/app/core/reducers/userInfo-reducer.ts
+++ src/app/core/reducers/userInfo-reducer.ts
@@ -4,12 +4,11 @@ export function userInfoReducer (state: UserInfoState, action: Action) {
   switch (action.type) {
     case 'setUserInfo':
       try {
-        // let roleAllowPath = [];
-        // action['payload'].roles.map(role => {
-        //   role.roleJsonInfo && (roleAllowPath = roleAllowPath.concat(role.roleJsonInfo.split(',')));
-        // });
-        // action['payload']['roleAllowPath'] = Array.from(new Set(roleAllowPath)).join(',');
-        action['payload']['roleAllowPath'] = "/home,/home/customer/potential,/home/customer/nointention,/home/visit/clue,/home/visit/nocard,/home/visit/member";
+        let roleAllowPath = [];
+        action['payload'].roles.map(role => {
+          role.roleJsonInfo && (roleAllowPath = roleAllowPath.concat(role.roleJsonInfo.split(',')));
+        });
+        action['payload']['roleAllowPath'] = Array.from(new Set(roleAllowPath)).join(',');
         window.localStorage.setItem('userInfo', JSON.stringify(action['payload']));
         return action['payload'];
       } catch (error) {
