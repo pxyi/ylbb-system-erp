44ffb3e963b569518a622eba5956354106a677d3
phuhoang <phuhoang@sina.com>
2018-08-24 10:31:50 +0800

    开始客户列表-操作

--- src/app/modules/customer/list/consumption/consumption.component.ts
+++ src/app/modules/customer/list/consumption/consumption.component.ts
@@ -1,4 +1,4 @@
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpService } from 'src/app/ng-relax/services/http.service';
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 
@@ -9,10 +9,37 @@ import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 })
 export class ConsumptionComponent implements OnInit, OnDestroy {
 
+  baseFormGroup: FormGroup;
+  timesCountGroup: FormGroup;
+  singleTimeGroup: FormGroup;
+
   constructor(
     private http: HttpService,
     private fb: FormBuilder = new FormBuilder(),
   ) { 
+    this.baseFormGroup = this.fb.group({
+      name: [{value: null, disabled: true}, [Validators.required]],
+      nick: [{value: null, disabled: true}],
+      sex: [{ value: null, disabled: true }],
+      monthAge: [{ value: null, disabled: true }],
+      comment: []
+    });
+    this.timesCountGroup = this.fb.group({
+      cardId: [, [Validators.required]],
+      swimTeacherId: [, [Validators.required]],
+      commodityId: [, [Validators.required]],
+      consumption: [{value: null, disabled: true}, [Validators.required]],
+      ringId: [],
+      swimDuration: [],
+      temperaturePost: [],
+      weight: [],
+      temperature: [],
+      satisfaction: [],
+      consumeDate: [{value: null, disabled: true}]
+    });
+    this.singleTimeGroup = this.fb.group({
+
+    })
   }
 
   ngOnInit() {
