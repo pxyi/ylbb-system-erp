f7f73005c34ef063ae164eebdf46dfecf7610c72
phuhoang <phuhoang@sina.com>
2018-09-04 22:04:44 +0800

    显示上传文件格式为application/vnd.ms-excel

--- src/app/modules/customer/list/import/import.component.ts
+++ src/app/modules/customer/list/import/import.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-import',
@@ -8,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImportComponent implements OnInit {
 
-  fileList = [];
-
   constructor(
-    private http: HttpClient
+    private message: NzMessageService
   ) { }
 
   ngOnInit() {
+  }
+
+  uploadComplate(e) {
+    if (e.type === 'success') {
+      this.message.create(e.file.response.code == 1000 ? 'success' : 'warning', e.file.response.info);
+    }
+  }
+
+  save(): Promise<boolean> {
+    return new Promise(resolve => resolve(true));
   }
 
 }
