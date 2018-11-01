88d95446c2c34646188b5ccff125822e2089b6d2
phuhoang <phuhoang@sina.com>
2018-11-01 19:18:37 +0800

    人力资源开发完毕

--- src/app/modules/human-resources/achievements/statistics/statistics.component.ts
+++ src/app/modules/human-resources/achievements/statistics/statistics.component.ts
@@ -1,3 +1,5 @@
+import { DatePipe } from '@angular/common';
+import { HttpService } from './../../../../ng-relax/services/http.service';
 import { QueryNode } from 'src/app/ng-relax/components/query/query.component';
 import { Component, OnInit } from '@angular/core';
 
@@ -10,7 +12,10 @@ export class StatisticsComponent implements OnInit {
 
   nowDate: Date = new Date();
 
-  dataSet: any[] = [];
+  dataSet = {
+    header: [],
+    content: []
+  }
 
   queryNode: QueryNode[] = [
     { label: '统计方式', key: 'statictype', type: 'select', options: [{ name: '总计', id: 0 }, { name: '按日', id: 1 }], default: 0 },
@@ -18,11 +23,24 @@ export class StatisticsComponent implements OnInit {
     { label: '起始日期', key: 'date', valueKey: ['startDate', 'endDate'], type: 'rangepicker', default: [this.nowDate, this.nowDate] }
   ]
 
-  constructor() { }
+  constructor(
+    private http: HttpService,
+    private format: DatePipe
+  ) { 
+    this.getData({ statictype: 0, startDate: this.format.transform(this.nowDate, 'yyyy-MM-dd'), endDate: this.format.transform(this.nowDate, 'yyyy-MM-dd')});
+  }
 
   ngOnInit() {
   }
 
-  log(e) {console.log(e)}
+  loading: boolean;
+  getData(params) {
+    this.loading = true;
+    params.reportId = 41;
+    this.http.post('/report/query', { paramJson: JSON.stringify(params) }, false).then(res => {
+      this.dataSet = res.result;
+      this.loading = false;
+    }).catch(err => this.loading = false)
+  }
 
 }
