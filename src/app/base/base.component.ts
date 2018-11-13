87f6b9f12790e32d253c1f9dae00594da6c3dcc2
phuhoang <phuhoang@sina.com>
2018-11-13 18:32:02 +0800

    去除log

--- src/app/base/base.component.ts
+++ src/app/base/base.component.ts
@@ -44,7 +44,6 @@ export class BaseComponent implements OnInit {
   showDrawer: boolean;
   ngOnInit() {
     this.route.data.subscribe((res: { userInfo: UserInfoState }) => {
-      console.log(res)
       this.userInfo = res.userInfo;
       window.document.title = `${this.userInfo.store['shopName']}-鱼乐贝贝ERP管理系统`;
     })
