e33cb90a87dbb392a5f0521438ea0fed0dfa38a8
phuhoang <phuhoang@sina.com>
2018-11-21 19:29:23 +0800

    2018-11-21  修改商品管理bug

--- src/app/modules/commodity/list/update/update.component.ts
+++ src/app/modules/commodity/list/update/update.component.ts
@@ -32,21 +32,22 @@ export class UpdateComponent implements OnInit {
       id: [],
       name: [, [Validators.required], [this.nameAsyncValidator]],                  //	商品名称
       status: [0, [Validators.required]],                  // 商品状态 0:’启用’,1:’禁用’
-      cardTimesFlag: [1, [Validators.required]],                 // 是否计次消费 0:’是’,1:’否’
-      type: [1, [Validators.required]],                  //	商品类型
+      cardTimesFlag: [0, [Validators.required]],                 // 是否计次消费 0:’是’,1:’否’
+      chargeType: [1, [Validators.required]],                  //	商品类型
       integral: [0, [Validators.required]],                  // 是否积分 计积分 0:’不记积分’,1:’记积分’
       defaultTag: [0, [Validators.required]],                  // 是否设置为默认 是否默认 0:’否’,1:’是’
       introduction: [],                                        //	商品说明
     });
-    this.formTypeChange(this.formGroup.get('type').value);
-    this.formGroup.get('type').valueChanges.subscribe(val => this.formTypeChange(val) );
+    this.formTypeChange(this.formGroup.get('chargeType').value);
+    this.formGroup.get('chargeType').valueChanges.subscribe(val => this.formTypeChange(val) );
     
     this.commodityInfo.id && this.formGroup.patchValue(this.commodityInfo);
   }
 
   formTypeChange(val) {
     if (val == 0) {
-      this.formGroup.addControl('chargeType', new FormControl(this.commodityInfo.id ? this.commodityInfo.chargeType : null, [Validators.required]));
+      this.formGroup.patchValue({ cardTimesFlag: 1 });
+      this.formGroup.addControl('type', new FormControl(this.commodityInfo.id ? this.commodityInfo.type : null, [Validators.required]));
       this.formGroup.addControl('cardNum', new FormControl(this.commodityInfo.id ? this.commodityInfo.cardNum : 0));
 
       this.formGroup.removeControl('stockPrice');
@@ -54,12 +55,13 @@ export class UpdateComponent implements OnInit {
       this.formGroup.removeControl('inventory');
       this.formGroup.removeControl('commission');
     } else {
+      this.formGroup.patchValue({ cardTimesFlag: 0 });
       this.formGroup.addControl('stockPrice', new FormControl(this.commodityInfo.id ? this.commodityInfo.stockPrice : null, [Validators.required]));
       this.formGroup.addControl('price', new FormControl(this.commodityInfo.id ? this.commodityInfo.price : null, [Validators.required]));
       this.formGroup.addControl('inventory', new FormControl(this.commodityInfo.id ? this.commodityInfo.inventory : true, [Validators.required]));
       this.formGroup.addControl('commission', new FormControl(this.commodityInfo.id ? this.commodityInfo.commission : true, [Validators.required]));
 
-      this.formGroup.removeControl('chargeType');
+      this.formGroup.removeControl('type');
       this.formGroup.removeControl('cardNum');
     }
   }
