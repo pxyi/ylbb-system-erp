9dd12fe2d8bf79632a6670e99b06509cd338d4bd
phuhoang <phuhoang@sina.com>
2019-06-25 19:12:13 +0800

    修复返利数据

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@
 export const environment = {
   production: false,
   version: '0.0.0',
-  domain: 'http://qnewbss.beibeiyue.cn/new_bss',
+  domain: 'http://tnewbss.beibeiyue.cn/new_bss',
   domainWs: 'ws://101.200.177.83:8008/new_bss',
   domainPay: 'https://tpay.beibeiyue.com/pay',
   domainOss: 'https://oss.beibeiyue.com',
