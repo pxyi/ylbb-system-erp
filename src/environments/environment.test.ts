2f37b670e484cc6b58e3c69141d5649e0795cad5
赵志祥 <17853755106@163.com>
2019-07-11 15:54:20 +0800

    收银台预约结算付款时增加reserveId

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   version: '0.0.0',
   // domain: 'http://qnewbss.beibeiyue.cn/new_bss',
-  domain: 'http://192.168.1.108:8888',
+  domain: 'http://192.168.1.135:8888',
   domainWs: 'ws://101.200.177.83:8008/new_bss',
   domainPay: 'https://tpay.beibeiyue.com/pay',
   domainOss: 'https://oss.beibeiyue.com',
